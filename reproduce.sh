025c3b5384151dcfb52fbf9303f29e0feda3a30d
llorracc <Christopher.Carroll@jhu.edu>
2020-10-15 16:25:49 -0400

    Cleanup junk from Public/master

--- reproduce.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-scriptDir="$(dirname "$0")"
-
-./reproduce_compute.sh
-./reproduce_textual.sh
-
--- reproduce_min.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Reproduce whatever set of results that can be reproduced in a few minutes
-
-scriptDir="$(dirname "$0")"
-
-./reproduce_compute.sh # All BufferStockTheory results can be reproduced quickly
-
